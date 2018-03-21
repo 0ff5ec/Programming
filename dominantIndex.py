b5aa9159a2fc59c7b772f16d1fa19760a2a1818a
0ff5ec <offsec.akash@gmail.com>
2018-03-21 14:35:17 -0700

    added dominantIndex.py

---
+++ dominantIndex.py
@@ -0,0 +1,45 @@
+# _*_ coding = utf-8 _*_
+'''
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+
+Note:
+
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
+'''
+"""
+        :type nums: List[int]
+        :rtype: int
+        """
+class Solution(object):
+    def dominantIndex(self, nums):
+        n = sorted(nums)
+        for i in range(0,len(nums)-1):
+            if n[i] != 0:
+                if n[-1]//n[i] < 2:
+                    return -1
+        return nums.index(n[-1])
+
+if __name__ == "__main__":
+	sol = Solution()
+	nums = [0,1,4,10]
+	print(sol.dominantIndex(nums))
