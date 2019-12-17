f7517582b32b2261ba4b560fe9e33f6a166af98d
0ff5ec <offsec.akash@gmail.com>
2019-12-16 21:44:48 -0800

    739. Daily Temperatures

---
+++ dailyTemperature.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        ans = [0]*len(T)
+        stack = []
+        for i in range(len(T)-1, -1, -1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1] - i
+            stack.append(i)
+        return ans
